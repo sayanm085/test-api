9f36a170ecd3969b5b67a3eb0c0eab8b8565537a
Sayan Mondal <sayanm085@gmail.com>
2024-08-07 18:33:51 +0530

    app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from '../app.js';
 
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(5000, () => {
+    console.log('Server is running on http://localhost:5000');
     }
     );
