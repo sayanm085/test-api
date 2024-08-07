6b9b5874127279a242d1c88e3adda3895fb3a883
Sayan Mondal <sayanm085@gmail.com>
2024-08-07 22:43:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
 import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Set the views directory
-app.set('views', path.join(__dirname, 'views'));
-
 // Set the view engine (e.g., EJS, Pug, etc.)
 app.set('view engine', 'ejs');
+// Set the path to the views directory
+app.set('views', path.join(__dirname, 'views'));
 
 // Define a route that renders the "login" view
-app.get('/login', (req, res) => {
+app.get('/', (req, res) => {
   res.render('login');
 });
 
